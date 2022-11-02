6d08e96f5cfe590017e3006d6e74c4a731e4f1a7
Alexander <zhukoff.alexander@gmail.com>
2022-11-02 14:54:06 +0300

    feat: Make the creation of pages on the server using api

--- src/script.js
+++ src/script.js
@@ -1,11 +1,30 @@
 import $ from 'jquery';
 
-$.get(
-    './api',
-    (data) => {
-        data.forEach((file) => {
-            $('body').append(`<h2>${file}</h2>`);
-        });
-    },
-    'JSON'
-);
+function getPageList() {
+    $('h2').remove();
+    $.get(
+        './api',
+        (data) => {
+            data.forEach((file) => {
+                $('body').append(`<h2>${file}</h2>`);
+            });
+        },
+        'JSON'
+    );
+}
+
+getPageList();
+
+$('button').click(() => {
+    $.post(
+        './api/createNewPage.php',
+        {
+            name: $('input').val(),
+        },
+        () => {
+            getPageList();
+        }
+    ).fail(() => {
+        alert('Страница уже существует');
+    });
+});
